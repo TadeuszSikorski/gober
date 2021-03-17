0b353e73a6867edb2d5c1b9ff0c2986fac5c1130
TadeuszSikorski <sikorskitadeusz@yahoo.com>
2021-03-17 09:04:22 +0100

    Initial commit

---
+++ src/main/java/io/github/tadeuszsikorski/gober/GoberApplication.java
@@ -0,0 +1,13 @@
+package io.github.tadeuszsikorski.gober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tadeuszsikorski/gober/GoberApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tadeuszsikorski.gober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
